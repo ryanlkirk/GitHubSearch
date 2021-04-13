1bee6933383a5bd8b73ac8c65b77feed15f44141
Ryan Kirk <33845055+ryanlkirk@users.noreply.github.com>
2021-04-13 21:46:28 +0100

    Add clear function

--- src/components/GitSearch.tsx
+++ src/components/GitSearch.tsx
@@ -15,132 +15,142 @@ import { Divider } from "@material-ui/core";
 import IRepos from "../types/custom";
 
 const GitSearch = () => {
-    const [profileImage, setProfileImage] = useState<string>("");
-    const [profileHeader, setProfileHeader] = useState<string>("");
-    const [repositories, setRepositories] = useState<IRepos[]>([]);
-    const [showRepos, setShowRepos] = useState<boolean>(false);
+	const [profileImage, setProfileImage] = useState<string>("");
+	const [profileHeader, setProfileHeader] = useState<string>("");
+	const [repositories, setRepositories] = useState<IRepos[]>([]);
+	const [showRepos, setShowRepos] = useState<boolean>(false);
+	const [searchValue, setSearchValue] = useState<string>("");
 
-    let fetchUser = () => {
-        var nameValue = (document.getElementById(
-            "site-search"
-        ) as HTMLInputElement).value;
+	let fetchUser = () => {
+		var nameValue = (document.getElementById("site-search") as HTMLInputElement)
+			.value;
 
-        fetch(`https://api.github.com/users/${nameValue}`)
-            .then(function (response) {
-                // The API call was successful!
-                return response.json();
-            })
-            .then(function (data) {
-                if (data.message && data.message === "Not Found") {
-                    setProfileHeader("No user found!");
-                } else {
-                    // This is the JSON from our response
-                    setProfileHeader(data.login);
-                    setProfileImage(data.avatar_url);
-                }
-                showRepos && setShowRepos(false);
-            })
-            .catch(function (err) {
-                // There was an error
-                console.warn("Something went wrong.", err);
-            });
+		fetch(`https://api.github.com/users/${nameValue}`)
+			.then(function (response) {
+				// The API call was successful!
+				return response.json();
+			})
+			.then(function (data) {
+				if (data.message && data.message === "Not Found") {
+					setProfileHeader("No user found!");
+				} else {
+					// This is the JSON from our response
+					setProfileHeader(data.login);
+					setProfileImage(data.avatar_url);
+				}
+				showRepos && setShowRepos(false);
+			})
+			.catch(function (err) {
+				// There was an error
+				console.warn("Something went wrong.", err);
+			});
 
-        //Fetch list of repos
-        fetch(`https://api.github.com/users/${nameValue}/repos`)
-            .then(function (response) {
-                // The API call was successful!
-                return response.json();
-            })
-            .then(function (data) {
-                // This is the JSON from our response
-                setRepositories(data);
-            })
-            .catch(function (err) {
-                // There was an error
-                console.warn("Something went wrong.", err);
-            });
-    };
+		//Fetch list of repos
+		fetch(`https://api.github.com/users/${nameValue}/repos`)
+			.then(function (response) {
+				// The API call was successful!
+				return response.json();
+			})
+			.then(function (data) {
+				// This is the JSON from our response
+				setRepositories(data);
+			})
+			.catch(function (err) {
+				// There was an error
+				console.warn("Something went wrong.", err);
+			});
+	};
 
-    let repoArr: string[] = [];
+	let clearSearch = () => {
+		setProfileImage("");
+		setProfileHeader("");
+		setRepositories([]);
+		setSearchValue("");
+	};
 
-    let additionaProfileInfo = additionalInfo(repositories.length);
+	let repoArr: string[] = [];
 
-    repositories &&
-        repositories.length > 0 &&
-        repositories.forEach((element) => {
-            repoArr.push(element.name);
-        });
+	let additionaProfileInfo = additionalInfo(repositories.length);
 
-    return (
-        <section className="searchBar">
-            <article className="searchBarCard">
-                <label htmlFor="site-search">
-                    <Typography variant="h6" align="center">
-                        GitHub User Search
-                    </Typography>
-                </label>
-                <TextField
-                    type="search"
-                    id="site-search"
-                    name="q"
-                    aria-label="Search through site content"
-                    variant="outlined"
-                    placeholder="Username"
-                ></TextField>
+	repositories &&
+		repositories.length > 0 &&
+		repositories.forEach((element) => {
+			repoArr.push(element.name);
+		});
 
-                <Button onClick={fetchUser}>Search</Button>
-                {profileHeader && repositories && (
-                    <>
-                        <UserProfile
-                            title={profileHeader}
-                            picture={profileImage}
-                            repositories={repositories}
-                        />
+	return (
+		<section className="searchArea">
+			<article className="searchBarCard">
+				<label htmlFor="site-search">
+					<Typography variant="h6" align="center">
+						GitHub User Search
+					</Typography>
+				</label>
+				<TextField
+					value={searchValue}
+					id="site-search"
+					name="q"
+					aria-label="Search through site content"
+					variant="outlined"
+					placeholder="Username"
+					onChange={(e) => setSearchValue(e.target.value)}
+				></TextField>
+				{searchValue && (
+					<Button size="small" onClick={clearSearch}>
+						<span className="text-grey">Clear</span>
+					</Button>
+				)}
 
-                        {profileHeader !== "No user found!" && (
-                            <Button
-                                variant="outlined"
-                                color="primary"
-                                onClick={() => setShowRepos(!showRepos)}
-                            >
-                                {!showRepos
-                                    ? "Show Repositories"
-                                    : "Hide Repositories"}
-                            </Button>
-                        )}
-                    </>
-                )}
-            </article>
+				<Button onClick={fetchUser}>Search</Button>
 
-            {showRepos && (
-                <article className="searchBarCard right-content">
-                    <Typography align="center" variant="h6">
-                        Repositories
-                    </Typography>
-                    <Divider />
-                    {additionaProfileInfo && (
-                        <Typography variant="subtitle1" align="center">
-                            {additionaProfileInfo.repoText}
-                        </Typography>
-                    )}
+				{profileHeader && repositories && (
+					<>
+						<UserProfile
+							title={profileHeader}
+							picture={profileImage}
+							repositories={repositories}
+						/>
 
-                    <aside className="reposDisplay">
-                        {repoArr &&
-                            showRepos &&
-                            repoArr.map((repoName, id) => {
-                                return (
-                                    <span key={id} className="repos">
-                                        <Typography align="center">
-                                            {repoName}
-                                        </Typography>
-                                    </span>
-                                );
-                            })}
-                    </aside>
-                </article>
-            )}
-        </section>
-    );
+						{profileHeader !== "No user found!" && (
+							<Button
+								variant="outlined"
+								color="primary"
+								onClick={() => setShowRepos(!showRepos)}
+							>
+								{!showRepos ? "Show Repositories" : "Hide Repositories"}
+							</Button>
+						)}
+					</>
+				)}
+			</article>
+
+			{showRepos && (
+				<article className="searchBarCard right-content">
+					<Typography align="center" variant="h6">
+						Repositories
+					</Typography>
+					<Divider />
+					{additionaProfileInfo && (
+						<Typography variant="subtitle1" align="center">
+							{additionaProfileInfo.repoText}
+						</Typography>
+					)}
+
+					<aside className="reposDisplay">
+						{repoArr &&
+							showRepos &&
+							repoArr.map((repoName, id) => {
+								return (
+									<span key={id} className="repos">
+										<Typography align="center">{repoName}</Typography>
+									</span>
+								);
+							})}
+					</aside>
+				</article>
+			)}
+		</section>
+	);
 };
 
 export default GitSearch;
